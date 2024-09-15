308feff83487dd085561af020dabd4b321788b85
Giorgi Vanadze <giorgivanadze03@gmail.com>
2024-09-15 16:57:37 +0400

    commit changes.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -35,7 +35,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -53,7 +53,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 DATABASES = {
